4761ad2fb787aec1cf9517701f6bfa0780e8eb90
tinhkohack <ductindongthap@gmail.com>
2023-03-11 22:07:53 +0700

    Duc Tin first commit

---
+++ header.h
@@ -0,0 +1,64 @@
+#ifndef _NSTRUCT_H
+#define _NSTRUCT_H
+#include <string>
+using namespace std;
+struct BirthDate{
+    int day;
+    int month;
+    int year;
+};
+struct dDate{
+    string day;
+    string session;
+};
+struct Scoreboard{
+    int No;
+    string StudentID;
+    string StudentName;
+    double OtherMark;
+    double Midterm;
+    double Final;
+    Scoreboard* nextBoard = nullptr;
+};
+
+struct Course{
+    string courseID;
+    string courseName;
+    string className;
+    string Teacher;
+    int credit;
+    int maxStudent;
+    dDate CourseDate;
+    Scoreboard* thisCourseScore = nullptr;
+    double ovrGPA; 
+    Course* nextCourse = nullptr; 
+};
+struct Semester{
+    int semesterIndex;
+    Course* Courselist;
+    double GPA;
+};
+struct Info{
+    string StudentID;
+    string FirstName;
+    string LastName;
+    bool isMale;
+    BirthDate Birth;
+    string SocialID;
+};
+struct Student{
+    int No;
+    Info This_Student;
+    string mainClass;
+    Semester* SemesterList = nullptr;
+    Student* nextStudent = nullptr;
+};
+struct StudyClass{
+    Student* pStudent = nullptr;
+    StudyClass* nextClass = nullptr;
+};
+struct Schoolyear{
+    StudyClass* pClass = nullptr;
+    Schoolyear* nextYear = nullptr;
+};
+#endif
