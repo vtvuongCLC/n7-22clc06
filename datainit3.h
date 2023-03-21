b76873d690eebd672359ed08c28ae464af221dad
Hoangcoderne <127637740+Hoangcoderne@users.noreply.github.com>
2023-03-22 00:42:38 +0700

    Update datainit3.h

--- datainit3.h
+++ datainit3.h
@@ -1,6 +1,12 @@
 #ifdef _DATAINIT_H
 #define _DATAINIT_H
 
+<<<<<<< Updated upstream
 
 
+=======
+bool pass_student(Student* user,string &pass);
+bool login_student(string &id,string &pass, Student* user);
+int login(Student* user);
+>>>>>>> Stashed changes
 #endif
