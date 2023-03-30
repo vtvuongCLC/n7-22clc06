8f323cd145588c08299335e5b3ef619186543c52
22127433 <anhtus2711@gmail.com>
2023-03-30 19:40:55 +0700

    update the function to edit information of course

--- editCourse.cpp
+++ editCourse.cpp
@@ -1,27 +1,27 @@
 #include "findObject.h"
 
-void viewInfoOfCourse(Course* thisCourse){
+void viewInfoOfCourse(CourseInfo infoThisCourse){
 
-    cout << left << setw(15) << thisCourse->courseID 
-             << left << setw(25) << thisCourse->courseName 
-             << left << setw(15) << thisCourse->className
-             << left << setw(25) << thisCourse->Teacher
-             << left << setw(3)  << thisCourse->credit 
-             << left << setw(4)  << thisCourse->maxStudent 
-             << left << setw(3)  << thisCourse->CourseDate.day
-                                 << ", " << thisCourse->CourseDate.session << endl;
+    cout << left << setw(15) << infoThisCourse.courseID 
+             << left << setw(25) << infoThisCourse.courseName 
+             << left << setw(15) << infoThisCourse.className
+             << left << setw(25) << infoThisCourse.Teacher
+             << left << setw(3)  << infoThisCourse.credit  
+             << left << setw(3)  << infoThisCourse.CourseDate.day
+                                 << ", " << infoThisCourse.CourseDate.session;
 
 }
 
 void viewListOfCourses(Semester* curSemester){ 
-    Course* pCourse = curSemester->firstCourse;
+    Course* pCourse = curSemester->CourseList;
     if(!pCourse){
         cout << "This semester hasn't yet been added course!!! First, please add the course to this semester";
         return;
     }
     cout << "The courses list in semester " << curSemester->semesterIndex << endl << endl;
     while(pCourse){
-        viewInfoOfCourse(pCourse);
+        viewInfoOfCourse(pCourse->infoThisCourse);
+        cout << left << setw(4)  << pCourse->maxStudent << endl;
         pCourse = pCourse->nextCourse;
     }
 }
@@ -34,16 +34,16 @@ void viewAllListOfCourses(Schoolyear* firstYear){
              << left << setw(25) << "Course Name" 
              << left << setw(15) << "Class Name"
              << left << setw(25) << "Teacher"
-             << left << setw(3)  << "Credit" 
-             << left << setw(4)  << "Max Students" 
-             << left << setw(3)  << "Day, Session" << endl;
+             << left << setw(3)  << "Credit"  
+             << left << setw(3)  << "Day, Session" 
+             << left << setw(4)  << "Max Students" << endl;
 
-        Semester* firstSemester = firstYear->firstSem;
+        Semester* firstSemester = firstYear->SemesterList;
 
         while(firstSemester){
 
-            viewListCourses(firstSemester);
-            firstSemester = firstSemester->nextSem;
+            viewListOfCourses(firstSemester);
+            firstSemester = firstSemester->nextSemester;
         }
         firstYear = firstYear->nextYear;
     }
@@ -51,15 +51,133 @@ void viewAllListOfCourses(Schoolyear* firstYear){
 
 Course* findTheCourse(Course* pCourse, string NameCourse, string IDCourse, string NameClass){
     while(pCourse){
-        if(NameCourse == pCourse->courseName 
-            && IDCourse == pCourse->courseID 
-            && NameClass == pCourse->className) 
+        if(NameCourse == pCourse->infoThisCourse.courseName 
+            && IDCourse == pCourse->infoThisCourse.courseID 
+            && NameClass == pCourse->infoThisCourse.className) 
         break;
         pCourse = pCourse->nextCourse;
     }
     return pCourse;
 } 
 
+void updateCourseInformation(Semester* curSemester){
+
+    string courseName, courseID, className;
+    cout << "Enter the course name you want to add student: ";
+    getline(cin, courseName);
+
+    cout << "Enter the course ID you want to add student: ";
+    getline(cin, courseID);
+
+    cout << "Enter the class name of course you want to add student: ";
+    getline(cin, className);
+
+    Course* thisCourse = findTheCourse(curSemester->CourseList, courseName, courseID, className);
+
+    if(thisCourse){
+        while(true){
+
+            cout << left << setw(15) << "Course ID" 
+                 << left << setw(25) << "Course Name" 
+                 << left << setw(15) << "Class Name"
+                 << left << setw(25) << "Teacher"
+                 << left << setw(3)  << "Credit"  
+                 << left << setw(3)  << "Day, Session" 
+                 << left << setw(4)  << "Max Students" << endl;
+             
+            viewInfoOfCourse(thisCourse);
+            
+            cout << endl;
+            cout << "The current course name is " << thisCourse->infoThisCourse.courseName << endl;
+            cout << "The current course ID is " << thisCourse->infoThisCourse.courseID << endl;
+            cout << "The current class name is " << thisCourse->infoThisCourse.className << endl;
+            cout << "The teacher name is " << thisCourse->infoThisCourse.Teacher << endl;
+            cout << "The number of credits is " << thisCourse->infoThisCourse.credit << endl;
+            cout << "The day of week and the session that the course will be performed are " << thisCourse->infoThisCourse.CourseDate.day << ", "
+                                                                                             << thisCourse->infoThisCourse.CourseDate.session << endl;
+            cout << "The maximum number of students in the course is " << thisCourse->maxStudent << endl;
+            cout << "0.Update finish\n1.Course name\n2.Course ID\n3.Class name\n4.Credits\n5.Day and session\n6.Max Students\nEnter the choice number to edit: ";
+            
+            int choice; cin >> choice;
+            cin.ignore();
+
+            switch(choice){
+                case 0: return;
+                case 1: 
+                    cout << "Enter the new course name: ";
+                    getline(cin, thisCourse->infoThisCourse.courseName);
+                    break;
+                case 2:
+                    cout << "Enter the new course ID: ";
+                    getline(cin, thisCourse->infoThisCourse.courseID);
+                    break;
+                case 3:
+                    cout << "Enter the new class name: ";
+                    getline(cin, thisCourse->infoThisCourse.className);
+                    break;
+                case 4:
+                    cout << "Enter the new credit: ";
+                    do{
+                        cin >> thisCourse->infoThisCourse.credit;
+                        if(thisCourse->infoThisCourse.credit <= 0) cout << "The number of credits must > 0\n";
+                    }while(thisCourse->infoThisCourse.credit <= 0);
+                    cin.ignore();
+                    break;
+                case 5:
+                    cout << "< 2 or > 7. Go back"   << endl
+                         << "2. Monday"    << endl
+                         << "3. Tueday"    << endl
+                         << "4. Wednesday" << endl
+                         << "5. Thursday"  << endl
+                         << "6. Friday"    << endl
+                         << "7. Saturday"  << endl
+                         << "Enter the selection of day of the week: ";
+
+                         int day; cin >> day;
+
+                         switch(day){
+                            case 2: thisCourse->infoThisCourse.CourseDate.day = "MON"; break;
+                            case 3: thisCourse->infoThisCourse.CourseDate.day = "TUE"; break;
+                            case 4: thisCourse->infoThisCourse.CourseDate.day = "WED"; break;
+                            case 5: thisCourse->infoThisCourse.CourseDate.day = "THU"; break;
+                            case 6: thisCourse->infoThisCourse.CourseDate.day = "FRI"; break;
+                            case 7: thisCourse->infoThisCourse.CourseDate.day = "SAT"; break;
+                            default: break;
+                         }
+
+                    cout << "< 1 or > 4. Go back" << endl
+                         << "1. S1 (07:30)" << endl
+                         << "2. S2 (09:30)" << endl
+                         << "3. S3 (13:30)" << endl
+                         << "4. S4 (15:30)" << endl
+                         << "Enter the selection of the session: ";
+                         
+                         int session; cin >> session;
+
+                         switch(day){
+                            case 1: thisCourse->infoThisCourse.CourseDate.session = "S1 (07:30)"; break;
+                            case 2: thisCourse->infoThisCourse.CourseDate.session = "S2 (09:30)"; break;
+                            case 3: thisCourse->infoThisCourse.CourseDate.session = "S3 (13:30)"; break;
+                            case 4: thisCourse->infoThisCourse.CourseDate.session = "S4 (15:30)"; break;
+                            default: break;
+                         }
+                         cin.ignore();
+                    break;
+                case 6:
+                    int numMaxStudents;
+                    cout << "Enter the number of max students (must bigger than current): ";
+                    do{
+                        cin >> numMaxStudents;
+                    } while(numMaxStudents < thisCourse->maxStudent);
+
+                    thisCourse->maxStudent = numMaxStudents;
+                    cin.ignore();
+            }
+            cout << "\nThe new update course information\n";
+        }
+    }
+    else cout << "This course doesn't exist.\n";
+}
 
 // StudyClass* findStudyClass(StudyClass* pClass, string name){
 //     while(pClass){
