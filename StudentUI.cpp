2ed3047d4af7da0bb2d175c92d59036f7d660bf0
Hoangcoderne <127637740+Hoangcoderne@users.noreply.github.com>
2023-04-22 19:52:16 +0700

    EnrollCourse

--- StudentUI.cpp
+++ StudentUI.cpp
@@ -15,6 +15,7 @@ void StudentUI(Student* curStudent, DataBase DB){
 
         switch(selection){
             case 0: return;
+
             case 1:
                 DisplayInfoStudent(curStudent);
                 break;
@@ -22,11 +23,61 @@ void StudentUI(Student* curStudent, DataBase DB){
                 ChangePasswordStudent(curStudent);
                 break;
             case 3:
-                
+            {   
+                char choice ;
+                EnrolledCourse* curListCourse = curStudent->lastEnrolledCourse;
+                int semester= curListCourse->ptoCourse->semester;
+                string year= curListCourse->ptoCourse->year;
+                DisplayCourseList1Student(curStudent, semester,year);
+                while(curListCourse->ptoCourse!=nullptr){
+                    bool display = false;
+                    if(semester!=curListCourse->ptoCourse->semester){
+                        display=true;
+                        semester=curListCourse->ptoCourse->semester;
+                    }
+                    if(year!=curListCourse->ptoCourse->year){
+                        display=true;
+                        year=curListCourse->ptoCourse->year;
+                    }
+                    if(display){
+                        cout<<"See More List Courses in another semester or year[Y/N]: ";
+                        cin>>choice;
+                        if(choice=='y' || choice=='Y')
+                            DisplayCourseList1Student(curStudent, semester,year);
+                        else break;
+                    }
+                    curListCourse=curListCourse->prevCourse;    
+                }
                 break;
+            }
             case 4:
-
+            {    
+                char choice ;
+                EnrolledCourse* curListCourse = curStudent->lastEnrolledCourse;
+                int semester= curListCourse->ptoCourse->semester;
+                string year= curListCourse->ptoCourse->year;
+                DisplayCourseList1Student(curStudent, semester,year);
+                while(curListCourse->ptoCourse!=nullptr){
+                    bool display= false;
+                    if(semester!=curListCourse->ptoCourse->semester){
+                        display=true;
+                        semester=curListCourse->ptoCourse->semester;
+                    }
+                    if(year!=curListCourse->ptoCourse->year){
+                        display=true;
+                        year=curListCourse->ptoCourse->year;
+                    }
+                    if(display){
+                        cout<<"See More Score board in another semester or year[Y/N]: ";
+                        cin>>choice;
+                        if(choice=='y' || choice=='Y')
+                            DisplayScoreboard1Student(curStudent, semester,year);
+                        else break;
+                    }
+                    curListCourse=curListCourse->prevCourse;    
+                }
                 break;
+            }
         }
     }
 }
