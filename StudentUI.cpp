0d5e1fcf977259b18365d37b552687dabffbd76d
Hoangcoderne <127637740+Hoangcoderne@users.noreply.github.com>
2023-04-23 13:16:25 +0700

    Fix StudentUI

--- StudentUI.cpp
+++ StudentUI.cpp
@@ -1,83 +1,85 @@
 #include "header/displayfunction.h"
 #include "header/loginfunction.h"
 #include "header/studentui.h"
+#include "header/ultilityfunction.h"
 
-void StudentUI(Student* curStudent, DataBase DB){
+void StudentUI(Student *curStudent, DataBase DB){
     int selection;
-    while(true){
+    while (true)
+    {
         system("cls");
         cout << "1. View profile info" << endl;
         cout << "2. Change password" << endl;
-        cout << "3. View enrolled courses" << endl;
-        cout << "4. View scoreboard" << endl;
-        cout << "0. Back" << endl << endl;
-        cout << ">> "; cin >> selection;
-
-        switch(selection){
-            case 0: return;
-
-            case 1:
-                DisplayInfoStudent(curStudent);
-                break;
-            case 2:
-                ChangePasswordStudent(curStudent);
-                break;
-            case 3:
-            {   
-                char choice ;
-                EnrolledCourse* curListCourse = curStudent->lastEnrolledCourse;
-                int semester= curListCourse->ptoCourse->semester;
-                string year= curListCourse->ptoCourse->year;
-                DisplayCourseList1Student(curStudent, semester,year);
-                while(curListCourse->ptoCourse!=nullptr){
-                    bool display = false;
-                    if(semester!=curListCourse->ptoCourse->semester){
-                        display=true;
-                        semester=curListCourse->ptoCourse->semester;
+        cout << "3. View enrolled courses and scoreboard" << endl;
+        cout << "0. Back" << endl
+             << endl;
+        cout << ">> ";
+        cin >> selection;
+        switch (selection)
+        {
+        case 0:
+            return;
+        case 1:
+            DisplayInfoStudent(curStudent);
+            system("pause");
+            break;
+        case 2:
+            ChangePasswordStudent(curStudent);
+            system("pause");
+            break;
+        case 3:
+            while (true)
+            {
+                int maxChoice = 0, choice;
+                system("cls");
+                cout << "Choose the year to view enrolled courses and scoreboard" << endl
+                     << endl;
+                cout << "List of years:" << endl;
+                DisplayYearList(DB.YearList, maxChoice);
+                cout << "0. Back" << endl
+                     << ">> ";
+                cin >> choice;
+                if (choice == 0)
+                    break;
+                if (choice > 0 && choice <= maxChoice)
+                {
+                    Schoolyear *chosenYear = navigateYear(DB.YearList, choice);
+                    system("cls");
+                    Semester **handlingArr = nullptr;
+                    cout << "Choose the semester of year " << chosenYear->year << " to view enrolled courses and scoreboard" << endl << endl;
+                    cout << left << setw(10) << "Index";
+                    cout << left << setw(10) << "Semester";
+                    cout << left << setw(10) << "Start";
+                    cout << left << setw(10) << "End";
+                    cout << endl;
+                    if (DisplaySemesterList(DB.SemesterList, handlingArr, chosenYear->year, maxChoice) == false)
+                        cout << "No semesters found." << endl;
+                    cout << endl;
+                    cout << "0. Back" << endl
+                         << ">> ";
+                    cin >> choice;
+                    if (choice == 0)
+                        continue;
+                    if (choice > 0 && choice <= maxChoice){
+                        Semester* chosenSemester = handlingArr[choice - 1];
+                        while (true){
+                            system("cls");
+                            cout << "Year: " << chosenYear->year << endl;
+                            cout << "Semester: " << chosenSemester->semester << endl;
+                            cout << "1. View enrolled courses" << endl;
+                            cout << "2. View scoreboard" << endl;
+                            cout << "0. Back" << endl
+                                 << ">> ";
+                            cin >> choice;
+                            if (choice == 0) break;
+                            if(choice == 1) DisplayCourseList1Student(curStudent, chosenSemester->semester, chosenYear->year);
+                            if(choice == 2) DisplayScoreboard1Student(curStudent, chosenSemester->semester, chosenYear->year);
+                            system("pause");
+                        }
                     }
-                    if(year!=curListCourse->ptoCourse->year){
-                        display=true;
-                        year=curListCourse->ptoCourse->year;
-                    }
-                    if(display){
-                        cout<<"See More List Courses in another semester or year[Y/N]: ";
-                        cin>>choice;
-                        if(choice=='y' || choice=='Y')
-                            DisplayCourseList1Student(curStudent, semester,year);
-                        else break;
-                    }
-                    curListCourse=curListCourse->prevCourse;    
                 }
-                break;
             }
-            case 4:
-            {    
-                char choice ;
-                EnrolledCourse* curListCourse = curStudent->lastEnrolledCourse;
-                int semester= curListCourse->ptoCourse->semester;
-                string year= curListCourse->ptoCourse->year;
-                DisplayCourseList1Student(curStudent, semester,year);
-                while(curListCourse->ptoCourse!=nullptr){
-                    bool display= false;
-                    if(semester!=curListCourse->ptoCourse->semester){
-                        display=true;
-                        semester=curListCourse->ptoCourse->semester;
-                    }
-                    if(year!=curListCourse->ptoCourse->year){
-                        display=true;
-                        year=curListCourse->ptoCourse->year;
-                    }
-                    if(display){
-                        cout<<"See More Score board in another semester or year[Y/N]: ";
-                        cin>>choice;
-                        if(choice=='y' || choice=='Y')
-                            DisplayScoreboard1Student(curStudent, semester,year);
-                        else break;
-                    }
-                    curListCourse=curListCourse->prevCourse;    
-                }
-                break;
-            }
+            break;
         }
     }
 }
